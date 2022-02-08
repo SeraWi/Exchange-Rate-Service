88961e96876ccee5cee9cbe9560cfc3351bf5d7d
SeraWi <wsr2357@naver.com>
2022-02-08 15:24:46 +0900

    Test Case 작성

--- ExchangeRateService/ExRateService/src/test/java/com/sera/exrate/controller/ExrateControllerTest.java
+++ ExchangeRateService/ExRateService/src/test/java/com/sera/exrate/controller/ExrateControllerTest.java
@@ -1,49 +1,57 @@
 package com.sera.exrate.controller;
 
-import static org.junit.Assert.*;
+
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.apache.taglibs.standard.tag.common.core.SetSupport;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
+import com.sera.exrate.domain.CurrencyResponseDto;
 import com.sera.exrate.service.CurrencyApiService;
-
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations= {"file:src/main/webapp/WEB-INF/spring/root-context.xml"})
-public class IndexControllerTest{
-
-	private final static Logger logger = LoggerFactory.getLogger(IndexControllerTest.class);
+public class ExrateControllerTest {
+	
+	@Mock
+	CurrencyResponseDto response;
+	
+	@Mock
+	CurrencyApiService service;
 	
 	@InjectMocks
-	IndexController controller;
+	ExrateController controller;
+	
+	//@Autowired
+	MockMvc mockMvc;
 	
 	@Before
-	public void setUpMockMvc() {
+	public void setUp() {
 		System.out.println("before");
-		MockitoAnnotations.initMocks(this);
-		//service = new CurrencyApiService();
+		//MockitoAnnotations.initMocks(this);
+		mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
 
 	}
 	
 	@Test
-	public void test(){
-		controller.index();
+	public void test() throws Exception {
 		
+		//when( controller.getExrate("KRW")).thenReturn(response);
+		
+		mockMvc.perform(get("/getExrate&currencies=KRW"))
+		.andExpect(status().isOk());
 	}
+
 
 }
--- ExchangeRateService/ExRateService/src/test/java/com/sera/exrate/service/CurrencyApiServiceTest.java
+++ ExchangeRateService/ExRateService/src/test/java/com/sera/exrate/service/CurrencyApiServiceTest.java
@@ -1,40 +1,52 @@
 package com.sera.exrate.service;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.sera.exrate.domain.CurrencyResponseDto;
+import com.sera.exrate.exception.CurrencyApiException;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations= {"file:src/main/webapp/WEB-INF/spring/root-context.xml"})
 public class CurrencyApiServiceTest {
 
-
 	@InjectMocks
 	CurrencyApiService service;
-	
+
 	@Mock
 	CurrencyResponseDto response;
 	
+	private final static Logger logger = LoggerFactory.getLogger(CurrencyApiService.class);
+
+	@Before
+	public void setUp() {
+		System.out.println("before");
+		MockitoAnnotations.initMocks(this);
+	}
+
 	@Test
 	public void test() throws Exception {
-		if(service ==null) {
-			System.out.println("service null");
-		}
-		if(response == null) {
-			System.out.println("response null");
-		}
-		
-		Mockito.when(service.getCurrencyInfo("KRW")).thenReturn(response);
-		
+		logger.info("service test");
+		response = service.getCurrencyInfo("PHP");
 		assertEquals(response.isSuccess(),true);
+	}
+	
+	@Test
+	public void exceptionTest() throws Exception {
+		logger.info("service exceptoin test");
+		when(service.getCurrencyInfo("")).thenThrow(new CurrencyApiException());
 	}
 
 }
